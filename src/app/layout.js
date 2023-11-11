ba0d974e83a9c82f28ea5a5b4f994a3f519c87a9
James T. Harding <59349643+empowermint@users.noreply.github.com>
2023-11-11 13:23:52 +0000

    Sketch header UI

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,10 @@
-import { Inter } from "next/font/google";
+import { Amatic_SC } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const font = Amatic_SC({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 export const metadata = {
   title: "Git Mountains",
@@ -11,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={font.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,29 @@
 import { Suspense } from "react";
 import MountainCanvas from "@/components/mountain-canvas";
 
-export default function Page() {
+export default function Page({ data }) {
+  const contributions = [0];
+  const user = "empowermint";
+
   return (
-    <main>
-      <Suspense fallback={"Loading..."}>
-        <MountainCanvas />
-      </Suspense>
-    </main>
+    <>
+      <header className="flex justify-between items-center p-4 tracking-wider">
+        <div className="">
+          <h1 className="text-3xl font-bold">Git Mountains</h1>
+        </div>
+        <div className="flex space-x-2">
+          <span className="text-xl">{user}</span>
+          <button className="text-s tracking-widest px-2 py-0 rounded-lg border-x-2 border-violet-400 hover:shadow-md hover:bg-violet-300">
+            Switch
+          </button>
+        </div>
+      </header>
+
+      <main>
+        <Suspense fallback={"Loading..."}>
+          <MountainCanvas data={contributions} />
+        </Suspense>
+      </main>
+    </>
   );
 }
