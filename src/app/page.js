f58222bfa640d32edf4930346a255aa501e1063b
James T. Harding <59349643+empowermint@users.noreply.github.com>
2023-11-12 16:31:41 +0000

    Configure UI to overlay `Canvas`

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import UserSelector from "@/components/user-selector";
 export default function HomePage() {
   return (
     <>
-      <div className="grid place-content-center place-items-center h-2/3">
+      <div className="grid place-content-center place-items-center h-full">
         <h2 className="text-8xl">Welcome</h2>
       </div>
     </>
